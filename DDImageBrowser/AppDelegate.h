8918be1078e56425dfc75cd4ed75de252d09d50a
DonnyYu <yudongdongcoder@gmail.com>
2016-03-10 12:09:16 +0800

    new create

---
+++ DDImageBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDImageBrowser
+//
+//  Created by mac on 16/3/10.
+//  Copyright © 2016年 dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDImageBrowser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DDImageBrowser
+//
+//  Created by mac on 16/3/10.
+//  Copyright © 2016年 dongdong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
